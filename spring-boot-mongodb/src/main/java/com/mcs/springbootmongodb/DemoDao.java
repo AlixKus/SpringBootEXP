b00b7a069c025d5b32ff333c9b93cb13e9dbd43c
mcs <787632628@qq.com>
2019-01-22 16:08:15 +0800

    Spring-boot-mongodb

---
+++ spring-boot-mongodb/src/main/java/com/mcs/springbootmongodb/DemoDao.java
@@ -0,0 +1,13 @@
+package com.mcs.springbootmongodb;
+
+public interface DemoDao {
+
+    void saveDemo(DemoEntity demoEntity);
+
+    void removeDemo(Long id);
+
+    void updateDemo(DemoEntity demoEntity);
+
+    DemoEntity findDemoById(Long id);
+
+}
---
+++ spring-boot-mongodb/src/test/java/com/mcs/springbootmongodb/DemoDaoTest.java
@@ -0,0 +1,62 @@
+package com.mcs.springbootmongodb;
+
+import net.minidev.json.JSONObject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringBootMongodbApplication.class)
+public class DemoDaoTest {
+    @Autowired
+    private DemoDao demoDao;
+
+    @Test
+    public void saveDemoTest() {
+
+        DemoEntity demoEntity = new DemoEntity();
+        demoEntity.setId(1L);
+        demoEntity.setTitle("Spring Boot 中使用 MongoDB");
+        demoEntity.setDescription("测试MongoDB");
+        demoEntity.setBy("mcs");
+        demoEntity.setUrl("http://www.MongoDB.com");
+
+        demoDao.saveDemo(demoEntity);
+
+        demoEntity = new DemoEntity();
+        demoEntity.setId(2L);
+        demoEntity.setTitle("Spring Boot 中使用 MongoDB");
+        demoEntity.setDescription("222");
+        demoEntity.setBy("mcs");
+        demoEntity.setUrl("http://www.spring.com");
+
+        demoDao.saveDemo(demoEntity);
+    }
+
+    @Test
+    public void removeDemoTest() {
+        demoDao.removeDemo(2L);
+    }
+
+    @Test
+    public void updateDemoTest() {
+        DemoEntity demoEntity = new DemoEntity();
+        demoEntity.setId(1L);
+        demoEntity.setTitle("Spring Boot 中使用 MongoDB 更新数据");
+        demoEntity.setDescription("Description");
+        demoEntity.setBy("mcs");
+        demoEntity.setUrl("http://www.mcs.com");
+
+        demoDao.updateDemo(demoEntity);
+    }
+
+    @Test
+    public void findDemoByIdTest() {
+
+        DemoEntity demoEntity = demoDao.findDemoById(1L);
+        System.out.println(demoEntity.toString());
+    }
+}
